e4a3b2800e1f067d1e5106929d08ca541177458e
Petro Radushynskyi <lsfreeman@ukr.net>
2020-09-08 00:00:40 +0300

    Fixed empty input in module-12

--- src/JS/fetchCountries.js
+++ src/JS/fetchCountries.js
@@ -1,7 +1,10 @@
 function fetchCountries(searchQuery) {
   const url = 'https://restcountries.eu/rest/v2/name/';
-
-  return fetch(url + searchQuery).then(response => response.json());
+  if(searchQuery) {
+    return fetch(url + searchQuery).then(response => response.json());
+  } else {
+    return;
+  }
 }
 
 export default fetchCountries;
